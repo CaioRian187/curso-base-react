a58b23aa49fd85767f57d5a35cc5f59b2f447977
CaioRian187 <caiorian246810@gmail.com>
2026-04-21 19:37:25 -0300

    Explicando conceitos sobre componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 
 export function App() {
 
   return (
-    <>
+    <div>
       Olá
-    </>
+    </div>
+  );
+}
+
+// Função JS
+const teste = () => {
+  return 1 + 1;
+}
+
+// Função JS
+const useTeste = () => {
+  return 1 + 1;
+}
+
+// Ract Hook
+const useTest = () => {
+  const [value] = useState(1 + 1);
+  return value;
+}
+
+// Componente funcional
+const Test = () => {
+  return (
+    <div>Teste</div>
   );
 }
