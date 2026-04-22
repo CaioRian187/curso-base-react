56e591504da3a6e3d89c81046ae0a708259dc5c1
CaioRian187 <caiorian246810@gmail.com>
2026-04-22 18:47:34 -0300

    Estrutura basica de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from "react";
+import type React from "react";
+
+
+
+interface IcardProps {
+  title: string
+  children: React.ReactNode
+}
+
+const Card = (props: IcardProps) => {
+  return (
+    <div style={{ border: '1px solid black' }}>
+      <span>Title: {props.title}</span>
+      <div>
+        {props.children}
+      </div>
+      <div>
+        Footer
+      </div>
+    </div>
+
+  );
+}
+
+
+
+
 
 export function App() {
 
   return (
     <div>
       Olá
+
+      <p>Card: </p>
+      <Card title="Teste de Props">
+        Teste de Children
+      </Card>
+
+
     </div>
-  );
-}
-
-// Função JS
-const teste = () => {
-  return 1 + 1;
-}
-
-// Função JS
-const useTeste = () => {
-  return 1 + 1;
-}
-
-// Ract Hook
-const useTest = () => {
-  const [value] = useState(1 + 1);
-  return value;
-}
-
-// Componente funcional
-const Test = () => {
-  return (
-    <div>Teste</div>
   );
 }
