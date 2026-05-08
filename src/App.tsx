2bb90da2a1d43fb0265589a3bac9d994a1ffe608
CaioRian187 <caiorian246810@gmail.com>
2026-05-08 19:45:48 -0300

    Removendo e concluindo itens da lista

--- src/App.tsx
+++ src/App.tsx
@@ -6,44 +6,47 @@ export function App() {
 
   const [value, setValue] = useState('');
   const [list, setList] = useState([
-    { id: '1', label: 'Fazer Café' },
-    { id: '2', label: 'Fazer Café' },
-    { id: '3', label: 'Fazer Almoço' },
-    { id: '4', label: 'Fazer Merenda' },
+    { id: '1', label: 'Fazer Café', complete: false },
+    { id: '2', label: 'Fazer Café', complete: false },
+    { id: '3', label: 'Fazer Almoço', complete: false },
+    { id: '4', label: 'Fazer Merenda', complete: false },
   ]);
 
   return (
     <div>
 
-      {/* Organizando o input: 
-      setando o valeu do input para ser o mesmo do useState,
-      setando o evento para que o input,
-      atualizando o estado value sempre que o conteúdo do campo é alterado pelo usuário  
-      */}
       <input
         value={value}
         type="text"
         onChange={(event) => setValue(event.target.value)} />
 
-      {/* criando uma função que pega tudo que está na lista e adiciona um novo objeto referênciando o useStage value,
-          como o valor do input é o mesmo do useStage para limpar o input basta limpar o useStage
-      */}
       <button onClick={() => {
-        setList([...list, { id: (list.length + 1).toString(), label: value }]);
+        setList([...list, { id: (list.length + 1).toString(), label: value, complete: false }]);
         setValue('');
       }}>
         Adicionar
       </button>
 
-
       <ol>
-
         {list.map((listItemComId) => (
           <li key={listItemComId.id}>
             {listItemComId.label}
+
+            {listItemComId.complete ? 'Concluido' : ''}
+
+            <button onClick={
+              () => setList([
+                ...list
+                  .map(item => ({ ...item, complete: item.id === listItemComId.id ? true : item.complete }))
+              ])}>
+              Concluir
+            </button>
+
+            <button onClick={() => setList([...list.filter(item => item.id !== listItemComId.id)])}>
+              Remover
+            </button>
           </li>
         ))}
-
       </ol>
 
     </div>
