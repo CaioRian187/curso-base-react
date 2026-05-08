9be500bd762d87b42584f1f4a80c3134785d9931
CaioRian187 <caiorian246810@gmail.com>
2026-05-07 23:05:04 -0300

    Mostra e esconder elementos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
+import { Teste } from "./components/teste/Teste";
 
 export function App() {
 
-  // Utilizando o useState
-  const [count, setCount] = useState(0);
+  // Como mostrar ou esconder o elemento na tela
+
+  const [hide, setHide] = useState(false);
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>
-        {count}
-      </button>
-    </div>
 
+      {hide && <p>Teste 1</p>}
+      {!hide && <p>Teste 2</p>}
+
+
+      {hide && <div >
+        <h1>Div apareceu</h1>
+      </div>}
+
+      {hide && <Teste />}
+      <button onClick={() => setHide(!hide)}>
+        Clique Aqui
+      </button>
+
+    </div>
   );
 }
---
+++ src/components/teste/Teste.tsx
@@ -0,0 +1,10 @@
+import "./Teste.css"
+
+export function Teste() {
+
+    return (
+        <div className="container">
+            <h1>Foi renderizado essa div</h1>
+        </div>
+    );
+}
