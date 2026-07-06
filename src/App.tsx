9bf245aa51819233194c3ccb367f19c1a00c85ae
CaioRian187 <caiorian246810@gmail.com>
2026-07-06 12:38:40 -0300

    feat: implementando a logica de navegação entra rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { AppLayout } from "./shared/layout/AppLayout";
 
 export function App() {
   return (
-    <AppLayout>
-      <Home />
-    </AppLayout>
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<About />} />
+
+          <Route path="*" element={<Navigate to='/' />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
   );
 }
--- src/shared/layout/AppLayout.tsx
+++ src/shared/layout/AppLayout.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./AppLayout.css"
+import { NavLink } from "react-router-dom";
 
 
 export const AppLayout = ({ children }: React.PropsWithChildren) => {
     return (
         <div className="layout_base">
             <div className="layout_header">
-                <a>Página incial</a>
-                <a>Usuários</a>
+                <NavLink to='/'>Página incial</NavLink>
+                <NavLink to='/sobre'>Sobre</NavLink>
             </div>
 
             <hr className="layout_divider" />
