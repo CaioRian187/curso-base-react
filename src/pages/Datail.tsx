9b8739ffcedd72aeb9e2b97f8460a33b509481fd
CaioRian187 <caiorian246810@gmail.com>
2026-07-06 13:19:06 -0300

    Adicionando a logica das url na pagina detail

--- src/pages/Datail.tsx
+++ src/pages/Datail.tsx
@@ -1,15 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 import { PageLayout } from "../shared/layout/page-layout/PageLayout";
 
 export const Datail = () => {
 
     const { id } = useParams();
-
+    const [searchParams] = useSearchParams();
 
     return (
         <PageLayout title="Detalhes">
-            Datail {id}
+            Datail {id} {searchParams.get('filter')}
         </PageLayout>
     );
 }
