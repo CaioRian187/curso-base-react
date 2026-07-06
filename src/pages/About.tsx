fd9563a58885ca3c106931d46815c014ec5ab8ee
CaioRian187 <caiorian246810@gmail.com>
2026-07-06 12:37:12 -0300

    Adicionando a dependencia react-router-dom

---
+++ src/pages/About.tsx
@@ -0,0 +1,19 @@
+import { PageLayout } from "../shared/layout/page-layout/PageLayout";
+
+
+export const About = () => {
+
+    return (
+        <PageLayout title="Sobre">
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt id, nesciunt iste perspiciatis aspernatur amet officia, placeat repudiandae rerum veritatis repellendus saepe accusantium, ratione excepturi facilis. Consectetur voluptate vero maxime.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia autem quaerat reprehenderit debitis quo delectus doloremque neque vitae eum necessitatibus optio, totam quis in cum tempora illum alias ipsum quia.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto deserunt maiores pariatur, ea omnis ab vitae possimus at. Sequi eveniet distinctio ab veritatis ullam accusamus! Ullam mollitia tempora in molestiae.
+            </p>
+        </PageLayout>
+    );
+}
