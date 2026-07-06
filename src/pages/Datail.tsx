187965cf136e200d2b5021a8821d97f6ecd51d74
CaioRian187 <caiorian246810@gmail.com>
2026-07-06 12:47:32 -0300

    feat: Criando a pagina de detalhes

---
+++ src/pages/Datail.tsx
@@ -0,0 +1,11 @@
+import { PageLayout } from "../shared/layout/page-layout/PageLayout";
+
+
+
+export const Datail = () => {
+    return (
+        <PageLayout title="Detalhes">
+            Datail
+        </PageLayout>
+    );
+}
